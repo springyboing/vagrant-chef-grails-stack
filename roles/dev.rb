f2a3846e7bb6a2d4cd20281ca8ce786e4395b334
Tim Redding <tim.redding@tribalddb.co.uk>
2012-06-21 16:14:25 +0100

    Removed git sub modules file

--- roles/dev.rb
+++ roles/dev.rb
@@ -1,4 +1,4 @@
 name "dev"
 description "Sets up a java development enviroment"
-run_list "recipe[apt]", "recipe[java]", "recipe[grails]", "recipe[groovy]", "recipe[git]", "recipe[subversion]"
+run_list "recipe[apt]", "recipe[java]", "recipe[grails]", "recipe[git]", "recipe[subversion]"
 override_attributes "java" => { "jdk_version" => "6" }
